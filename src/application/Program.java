90ff4df8ae0b604938ae2c974518e6d19bd21eb8
Marcelo Caleb Julio Ofice <marcelocalebjulioo@gmail.com>
2025-01-30 18:14:05 +0200

    DELETE

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,13 +2,10 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
@@ -16,42 +13,22 @@ public class Program {
 		
 		Connection conn = null;
 		PreparedStatement st = null;
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+			
 		try {
+		
 			conn = DB.getConnection();
+			st = conn.prepareStatement("DELETE FROM department "
+					+ "WHERE "
+					+ "Id = ? ");
 			
-			st = conn.prepareStatement(
-					"INSERT INTO seller"
-					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)", 
-					Statement.RETURN_GENERATED_KEYS);
-			
-			st.setString(1, "Ofice Júlio");
-			st.setString(2, "ofice@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1940").getTime()));
-			st.setDouble(4, 3000.0);
-			st.setInt(5, 1);
+			st.setInt(1, 2);
 			
 			int rowsAffected = st.executeUpdate();
 			
-			if(rowsAffected > 0) {
-				ResultSet rs = st.getGeneratedKeys();
-				while(rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Done! Id = " + id);
-				}
-			}
-			else {
-				System.out.println("No rown affected!");
-			}
+			System.out.println("Done! Rows affected: " + rowsAffected);
 		}
 		catch(SQLException e) {
-			e.printStackTrace();
-		}catch(ParseException e) {
-			e.printStackTrace();
+			throw new DbIntegrityException(e.getMessage());
 		}
 		finally {
 			DB.closeStatement(st);

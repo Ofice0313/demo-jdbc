723630df2117550734b4e66f8aec7fa93cc866cf
Marcelo Caleb Julio Ofice <marcelocalebjulioo@gmail.com>
2025-01-30 17:31:53 +0200

    recover data from database

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,9 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.sql.*;
 
 import db.DB;
 
@@ -12,8 +11,29 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.createStatement();
+			
+			rs = st.executeQuery("select * from department");
+			
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeResultSet(rs);;
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
 	
 	}
 
